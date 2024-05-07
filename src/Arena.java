4270e26ad229d6433ef60ce731e7e2dc5d216ebe
victor <victorl_222@hotmail.com>
2024-05-07 18:00:04 -0300

    add function mostrarArena

--- src/Arena.java
+++ src/Arena.java
@@ -1,33 +1,34 @@
 public class Arena {
 
 
-    public char [][] arenaGrande;
-    public char [][] arenaMediana;
-    public char [][] arenaChica;
-
-public Arena(){
-    arenaGrande = new char[30][15];
-    arenaMediana = new char[15][10];
-    arenaChica = new char[10][10];
-}
+    public char [][] arena;
 
 
 
+    public Arena(){
+
+    }
 
 
+    public char[][] getArena() {
+        return arena;
+    }
 
-    public  void iniciarArena(char seleccionArena){
+    public  void iniciarArena(String seleccionArena){
         switch(seleccionArena){
-            case 'P':
+            case "P":
                 iniciarChica();
+                break;
 
-            case 'M':{
+            case "M":{
                 iniciarMediana();
-
+                break;
             }
-            case 'G':
+            case "G":
                 iniciarGrande();
-
+                break;
+            default:
+               System.out.println("Solo son validas las opciones P,M,G");
         }
 
 
@@ -36,47 +37,53 @@ public Arena(){
 
 
 
-    public void iniciarChica(){
-        arenaChica=new char[10][10];
-        for (int i = 0; i < arenaChica.length; i++) {
-            for (int j = 0; j < arenaChica.length; j++) {
-                arenaChica[i][j]='*';
+    public char [][] iniciarChica(){
+        arena=new char[10][10];
+        for (int i = 0; i < arena.length; i++) {
+            for (int j = 0; j < arena[0].length; j++) {
+                arena[i][j]='*';
             }
         }
+        return arena;
     }
 
 
-    public void iniciarMediana(){
-        arenaMediana=new char[15][10];
-        for (int i = 0; i < arenaMediana.length; i++) {
-            for (int j = 0; j < arenaMediana.length; j++) {
-                arenaMediana[i][j]='*';
+    public char [][] iniciarMediana(){
+        arena=new char[15][10];
+        for (int i = 0; i < arena.length; i++) {
+            for (int j = 0; j < arena[0].length; j++) {
+                arena[i][j]='*';
             }
         }
+        return arena;
     }
+
+
+
     public char[][] iniciarGrande(){
-    arenaGrande=new char[30][15];
-    for (int i = 0; i < arenaGrande.length; i++) {
-        for (int j = 0; j < arenaGrande.length; j++) {
-            arenaGrande[i][j]='*';
+        arena= new char[30][15];
+    for (int i = 0; i < arena.length; i++) {
+        for (int j = 0; j < arena[0].length; j++) {
+            arena[i][j]='*';
         }
     }
-    return arenaGrande;
+    return arena;
+
     }
 
 
 
-  //  public void mostrarArena(Arena arena){
-   //     for (int i = 0; i < ; i++) {
-     //       for (int j = 0; j < ; j++) {
-       //         System.out.print(chars[j] + "\t");
+   public void mostrarArena(char[][] arena){
+     for (int i = 0; i < arena.length ; i++) {
+         for (int j = 0; j < arena[0].length ; j++) {
+             System.out.print(arena[i][j] + "\t");
 
             }
-         //   System.out.println();
+           System.out.println();
 
-       // }
-   // }
-//}
+       }
+    }
+}
 
 
 
--- src/ClashRoyale.java
+++ src/ClashRoyale.java
@@ -6,13 +6,14 @@ public class ClashRoyale {
 
 
     public void jugar(){
-        char seleccionArena;
+        String seleccionArena;
         Scanner sc = new Scanner(System.in);
         System.out.print("Ingrese el arena del royale: ");
-        seleccionArena=sc.next().charAt(0);
+        seleccionArena=sc.next();
         Arena arena=new Arena();
         arena.iniciarArena(seleccionArena);
-        System.out.println(seleccionArena);
+        arena.mostrarArena(arena.getArena());
+
         //arena.mostrarArena(arena);
 
     }
